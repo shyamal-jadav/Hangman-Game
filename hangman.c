1ee03e75ec3a63b3d256d063d8902cfbd0c7185f
Siren <siren@kernal.eu>
2021-10-11 15:26:31 +0300

    Reformatting, minor improvements

--- hangman.c
+++ hangman.c
@@ -2,6 +2,7 @@
 #include <conio.h>
 #include <string.h>
 #include <stdlib.h>
+
 void logo()
 {
 	printf("\n\t\t\t    :::::::::::::::::::::::");
@@ -10,6 +11,7 @@ void logo()
 	printf("\n\t\t\t    :::                 :::");
 	printf("\n\t\t\t    :::::::::::::::::::::::");
 }
+
 void main()
 {
 	int i, c, count=0, flag=0, temp=0;
@@ -17,122 +19,104 @@ void main()
 	char *next, *moviename;
 	int movieindex;
 	time_t rdnseed;
-	srand((unsigned) time(&rdnseed));
-	system("cls");//clrscr();
+	srand((unsigned)time(&rdnseed));
+	system("cls"); //clrscr();
 	logo();
 
-	FILE * pFile;
+	FILE *pFile;
 	long lSize;
-	char * completelist;
-	pFile = fopen ( "movielist" , "rb" );
-	fseek (pFile , 0 , SEEK_END);
-	lSize = ftell (pFile);
-	rewind (pFile);
-	completelist = (char*) malloc (sizeof(char)*lSize);
-	fread (completelist, 1, lSize, pFile);
+	char *completelist;
+	pFile = fopen ("movielist" , "rb");
+	fseek(pFile , 0 , SEEK_END);
+	lSize = ftell(pFile);
+	rewind(pFile);
+	completelist = malloc(sizeof(char) * lSize);
+	fread(completelist, 1, lSize, pFile);
 	//printf("%s\n\n",completelist);
 	fclose (pFile);
 
-	movieindex=rand() % 90;
+	movieindex = rand() % 90;
 	next = strtok(completelist, "\n");
-	while(next != NULL)
-	{
-		if(movieindex == 0)
-		{
-			moviename=next;
-		}
+
+	while (next != NULL) {
+		if (movieindex == 0)
+			moviename = next;
+
 		next = strtok(NULL, "\n");
 		movieindex--;
 	}
 	//printf("\n%s\n",moviename);
 
-	for(c=0;c<50&&moviename[c]!=0;c++)
-	{
-		a[c]=moviename[c];
-	}
+	for (c = 0; c < 50 && moviename[c] != 0; c++)
+		a[c] = moviename[c];
 
 	printf("\n\n\n\t          ******************************************");
 	printf("\n\t          ..........................................\n");
-	for(i=0;i<38-(c/2);i++)
-	{
+
+	for (i = 0; i < 38 - (c / 2); i++)
 		printf(" ");
-	}
-	for(i=0;i<c;i++)
-	{
-		if( (a[i]>96 && a[i]<123) || (a[i]>64 && a[i]<91) )
-		{
+
+	for (i = 0; i < c; i++) {
+		if ((a[i] > 96 && a[i] < 123) || (a[i] > 64 && a[i] < 91))
 			printf("%c", d);
-		}
-		else{
+		else
 			printf("%c", a[i]);
-		}
-		b[i]=d;
+
+		b[i] = d;
 	}
+
 	printf("\n\t          ..........................................");
 	printf("\n\t          ******************************************\n\n");
 
-	while(count<c)
-	{
-		flag=0;
+	while (count < c) {
+		flag = 0;
 		printf("\n\n\n\n\t\tEnter any character: ");
 		x = getch();
-		for(i=0;i<c;i++)
-		{
-			if(x==a[i])
-			{
-				b[i]=a[i];
-				flag=1;
+
+		for (i = 0; i < c; i++) {
+			if(x == a[i]) {
+				b[i] = a[i];
+				flag = 1;
 				temp++;
 			}
 		}
+
 		system("cls");	//clrscr();
 		logo();
 		printf("\n\n\n\t          ******************************************");
 		printf("\n\t          ..........................................\n");
-		for(i=0;i<38-(c/2);i++)
-		{
+
+		for(i = 0; i < 38 - (c / 2); i++)
 			printf(" ");
-		}
-		for(i=0;i<c;i++)
-		{
-			if( (a[i]>96 && a[i]<123) || (a[i]>64 && a[i]<91) )
-			{
+
+		for(i=0;i<c;i++) {
+			if((a[i] > 96 && a[i] < 123) || (a[i] > 64 && a[i] < 91))
 				printf("%c", b[i]);
-			}
 			else
-			{
 				printf("%c", a[i]);
-			}
 		}
+
 		printf("\n\t          ..........................................");
 		printf("\n\t          ******************************************\n\n");
 
-		if(flag==0)
-		{
+		if (flag == 0) {
 			count++;
 			printf("\n\n\t\t\t      %c is a wrong guess", x);
 			printf("\n\t\t\t    you have %d more chance ", c-count);
 			printf("\n\t\t\t    %d more word remaining", c-temp);
-		}
-		else
-		{
+		} else {
 			count++;
 			printf("\n\n\t\t\t   *** you are correct ***");
-			printf("\n\t\t\t    you have %d more chance ",c-count);
+			printf("\n\t\t\t    you have %d more chance ", c-count);
 			printf("\n\t\t\t    %d more word remaining", c-temp);
 		}
 
-		if(temp==c)
-		{
+		if (temp == c)
 			break;
-		}
 	}
-	if(temp==c)
-	{
+	if (temp == c) {
 		printf("\n\n\n\t\t        :)  @*@*@* YOU WON *@*@*@  :)");
-	}
-	else
-	{
+	} else {
 		printf("\n\n\n\t\t        :( YOU LOSE :(");
 		printf("\n\t\t        :( YOU ARE HANGED :(");
 	}
